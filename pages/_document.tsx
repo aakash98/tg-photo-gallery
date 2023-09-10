517371fd8c32d649bb27d85c8764cec4f9ae2556
aakashmahadevan <aakash.mahadevan@spinny.com>
2023-09-10 23:48:53 +0530

    Fixing Link Descriptions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,22 +7,13 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/logo-png.ico" />
-          <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
+          <meta name="description" content="Trippy Gandalf Artfolio." />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
+          <meta property="og:description" content="Trippy Gandalf Artfolio." />
           <meta property="og:title" content="Trippy Gandalf Artfolio" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Trippy Gandalf Artfolio" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
+          <meta name="twitter:description" content="Trippy Gandalf Artfolio" />
         </Head>
         <body className="bg-black antialiased">
           <Main />
